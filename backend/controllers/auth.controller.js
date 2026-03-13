a82a925be19638d3f5a4216c20283a1527f077d8
Thomas Ho <khoaho120703@gmail.com>
2026-03-13 07:13:47 +0700

    [backend] feat: auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,1 +1,50 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import User from "../models/User.model.js";
+
+//Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
+};
+
+//@desc Register new user
+//@route POST /api/auth/register
+//@access Public
+export const register = async (req, register, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+//@desc Get user profile
+//@route GET /api/auth/profile
+//@access Private
+
+export const getProfile = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+//@desc Update user profile
+//@route PUT /api/auth/profile
+//@access Private
+export const updateProfile = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+//@desc Change password
+//@route POST /api/auth/change-password
+//@access Private
+export const changePassword = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 });
 
 //Start server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
